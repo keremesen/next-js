aba598014b46d0e4dd5bbf164513cc44f5a57688
Kerem Esen <kerem.esen.01@hotmail.com>
2021-02-27 23:46:12 +0300

    greatest scroll bar

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,108 @@
-import { Button, Flex, Heading } from "@chakra-ui/react";
+import { Button, Flex, Heading, Progress, Stack } from "@chakra-ui/react";
 import { useToast } from "@chakra-ui/react";
 import Lottie from "react-lottie";
-import animationData from '../public/43988-walkwalk.json'
+import animationData from "../public/43988-walkwalk.json";
+import animationData2 from "../public/19962-blog-page.json";
+import React, { useEffect, useState } from "react";
 
 export default function Home() {
-  const toast = useToast();
+  // const toast = useToast();
+
+  const [scrolled, setScrolled] = useState(0);
+
+  function onScroll() {
+    var winScroll = document.documentElement.scrollTop;
+    var height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    setScrolled((winScroll / height) * 100);
+  }
+  useEffect(() => {
+    window.onscroll = function () {
+      onScroll();
+    };
+  }, [onScroll]);
+
   return (
     <Flex
-      bgColor="tomato"
-      w="100vw"
-      h="100vh"
-      align="center"
-      justify="space-evenly"
+      justify="center"
       direction="column"
+      bgColor="gray.300"
+      height="10000px"
+      w="100%"
     >
-<Lottie width={200} height={200}
-options={{loop:true, autoplay:true, animationData}} ></Lottie>
+      <Progress
+        //isIndeterminate
+        colorScheme="blackAlpha"
+        bgGradient="linear-gradient(to right top, #d445cc, #be33ce, #a522d0, #8614d4, #5d0ed7)"
+        value={scrolled}
+        size="xs"
+        w="100%"
+        h="10px"
+        pos="sticky"
+        top="0"
+        zIndex="1"
+        borderRadius="6px"
+      />
+      <Flex bgColor="" w="1080px" h="100%" direction="column">
+        <Lottie
+          speed={1}
+          options={{
+            loop: true,
+            autoplay: true,
+            animationData: animationData2,
+          }}
+          width="500px"
+          height="500px"
+        ></Lottie>
+        <Lottie
+          speed={1}
+          options={{
+            loop: true,
+            autoplay: true,
+            animationData: animationData2,
+          }}
+          width="500px"
+          height="500px"
+        ></Lottie>
+        <Lottie
+          speed={1}
+          options={{
+            loop: true,
+            autoplay: true,
+            animationData: animationData2,
+          }}
+          width="500px"
+          height="500px"
+        ></Lottie>
+        <Lottie
+          speed={1}
+          options={{
+            loop: true,
+            autoplay: true,
+            animationData: animationData2,
+          }}
+          width="500px"
+          height="500px"
+        ></Lottie>
+        <Lottie
+          speed={1}
+          options={{
+            loop: true,
+            autoplay: true,
+            animationData: animationData2,
+          }}
+          width="500px"
+          height="500px"
+        ></Lottie>
+
+        <Lottie
+          speed={1}
+          options={{ loop: true, autoplay: true, animationData: animationData }}
+          width="500px"
+          height="500px"
+        ></Lottie>
+      </Flex>
       {/* <Heading>SARKI ONERME SITESI</Heading>
       <Button
         onClick={() =>
